a9135db93291c3c8d8e262c0dacb51b804e5fbb7
Martin Atkins <mart@degeneration.co.uk>
2012-12-30 09:26:41 -0800

    Add mechanical fasteners holes to the horizontal walls.
    These will mate with a strip of plastic that passes through all of the
    holes to allow the grid part to be connected to the base part such that
    it can be taken apart if necessary for maintenance.

--- enclosure/make_schematic.py
+++ enclosure/make_schematic.py
@@ -11,8 +11,13 @@ ledstrip_thickness = 2.0
 ledstrip_width = 10.0
 mitre_width = 10.0
 part_padding = 2
+fastener_bracket_thickness = 8.0
+fastener_bracket_curve_radius = 2
+fastener_tab_thickness = 3.0
+fastener_tab_width = 20.0
 
 from cadoodle import Drawing
+from math import ceil, floor
 
 d = Drawing()
 
@@ -74,20 +79,65 @@ def draw_horiz(horiz):
         - wall_thickness # slit for the perpendicular wall
         - mitre_width    # mitre to mate with the base
     )
+    cell_flush_bottom_fastener = (
+        cell_pitch
+        - (fastener_bracket_thickness * 2)
+        - fastener_tab_width
+        - wall_thickness # slit for the perpendicular wall
+    )
 
     horiz.move(0, (height / 2.0) + base_thickness)
+
+    # Two of the cells are special in that they poke through the base
+    # further to form the mechanism by which the grid fastens to the
+    # base such that it can be removed later for maintenence.
+    fastener_cell_idx = int(cells_horiz * 0.25)
+    fastener_cells = [
+        fastener_cell_idx,
+        cells_horiz - fastener_cell_idx - 1,
+    ]
 
     # Bottom edge
     horiz.east(wall_thickness / 2.0) # extra "lip" for the outer wall
     for i in xrange(0, cells_horiz):
+        is_fastener = i in fastener_cells
         # complete the slit for the perpendicular walls
         horiz.east(wall_thickness / 2.0)
         horiz.south(height / 2.0)
-        horiz.east(cell_flush_bottom / 2.0)
-        horiz.south(base_thickness)
-        horiz.east(mitre_width)
-        horiz.north(base_thickness)
-        horiz.east(cell_flush_bottom / 2.0)
+        if is_fastener:
+            horiz.east(cell_flush_bottom_fastener / 2.0)
+            horiz.south(base_thickness + fastener_bracket_thickness + fastener_tab_thickness - fastener_bracket_curve_radius)
+            horiz.curve_sw_ccw(fastener_bracket_curve_radius)
+            horiz.east(
+                fastener_tab_width
+                + (fastener_bracket_thickness * 2)
+                - (fastener_bracket_curve_radius * 2)
+            )
+            horiz.curve_se_ccw(fastener_bracket_curve_radius)
+            horiz.north(base_thickness + fastener_bracket_thickness + fastener_tab_thickness - fastener_bracket_curve_radius)
+
+            # Double back for a moment and draw the hole where the
+            # fastener tab will go.
+            horiz.move(
+                -(fastener_bracket_thickness + fastener_tab_width),
+                -base_thickness,
+            )
+            horiz.south(fastener_tab_thickness)
+            horiz.east(fastener_tab_width)
+            horiz.north(fastener_tab_thickness)
+            horiz.west(fastener_tab_width)
+            horiz.move(
+                (fastener_bracket_thickness + fastener_tab_width),
+                base_thickness,
+            )
+
+            horiz.east(cell_flush_bottom_fastener / 2.0)
+        else:
+            horiz.east(cell_flush_bottom / 2.0)
+            horiz.south(base_thickness)
+            horiz.east(mitre_width)
+            horiz.north(base_thickness)
+            horiz.east(cell_flush_bottom / 2.0)
         horiz.north(height / 2.0)
         horiz.east(wall_thickness / 2.0)
     horiz.east(wall_thickness / 2.0) # extra "lip" for the outer wall
@@ -102,7 +152,7 @@ def draw_horiz(horiz):
     horiz.south(height / 2.0)
 
 horiz.x = part_padding
-horiz.y = (part_padding * 2) + height + base_thickness
+horiz.y = (part_padding * 2) + height + base_thickness + fastener_tab_thickness + fastener_bracket_thickness
 draw_horiz(horiz)
 
 d.save("schematic.dxf")
